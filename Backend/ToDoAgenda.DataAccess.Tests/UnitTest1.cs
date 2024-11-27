49270c51490685ceee3a8d151b214f0ca264749e
onuraydi <onuraydi@outlook.com>
2024-11-27 09:49:35 +0300

    Katmanlar eklendi

---
+++ Backend/ToDoAgenda.DataAccess.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿namespace ToDoAgenda.DataAccess.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
