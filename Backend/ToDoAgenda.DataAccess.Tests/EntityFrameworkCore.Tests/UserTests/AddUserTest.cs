be246bfa11d47171bc3f3f46f839294e16632cdc
Onur Aydı <onuraydi@outlook.com>
2024-12-13 18:18:28 +0300

    User Ekleme testi yazıldı ve çalıştı

--- Backend/ToDoAgenda.DataAccess.Tests/EntityFrameworkCore.Tests/UserTests/AddUserTest.cs
+++ Backend/ToDoAgenda.DataAccess.Tests/EntityFrameworkCore.Tests/UserTests/AddUserTest.cs
@@ -1,6 +1,9 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using ToDoAgenda.DataAccess.Concrete.EntityFrameworkCore.ForEntity;
 using TODoAgenda.Entities.Concrete;
+using Assert = Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
+
+using Task = System.Threading.Tasks.Task;
 
 namespace ToDoAgenda.DataAccess.Tests;
 
@@ -8,22 +11,30 @@ namespace ToDoAgenda.DataAccess.Tests;
 public class AddUserTest
 {
     [TestMethod]
-    public async void Add_User()
+    public async Task Add_User()
     {
-
         // Arrange
         EfCoreUserDal userDal = new EfCoreUserDal();
-        User user = new User(); 
-        user.UserId = 1;
-        user.UserName = "deneme";
-        user.UserSurname = "deneme";
-        user.UserEmail = "deneme@gmail.com";
+        User user = new User
+        {
+            UserName = "deneme",
+            UserSurname = "deneme",
+            UserEmail = "deneme@gmail.com"
+        };
 
         // Act
         await userDal.Add(user);
-        
+
         // Assert
+        var items = await userDal.GetAll();
 
-
+        // Kullanýcýnýn özelliklerini karþýlaþtýrarak doðrulama yapýyoruz
+        var addedUser = items.FirstOrDefault(u => u.UserEmail == user.UserEmail);
+        Assert.IsNotNull(addedUser); // Kullanýcý eklenmiþ olmalý
+        Assert.AreEqual(user.UserName, addedUser.UserName); // Ýsimler eþit mi?
+        Assert.AreEqual(user.UserSurname, addedUser.UserSurname); // Soyad eþit mi?
+        Assert.AreEqual(user.UserEmail, addedUser.UserEmail); // E-mail eþit mi?
+        Assert.AreEqual(1, items.Count); // Listede sadece 1 kullanýcý olmalý
     }
+
 }
--- Backend/ToDoAgenda.DataAccess.Tests/obj/Debug/net9.0/ToDoAgenda.DataAccess.Tests.AssemblyInfo.cs
+++ Backend/ToDoAgenda.DataAccess.Tests/obj/Debug/net9.0/ToDoAgenda.DataAccess.Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToDoAgenda.DataAccess.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17dc43b47bc44b5871f550d34b62c953b3fbbc4f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0ee1d9214f1463027f57979cf24112fd9b2ead3")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToDoAgenda.DataAccess.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToDoAgenda.DataAccess.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
